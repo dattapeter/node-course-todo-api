e4c2fb10df4ba8f2cd44ec4d16574ceb7c8dd1cc
Dattatray <dattapeter@gmail.com>
2019-01-29 22:41:13 +0530

    ToDos Final

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -29,6 +30,20 @@ app.get('/todos', (req, res) => {
         res.status(400).send(err);
     })
 });
+
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then(todo => {
+        res.status(200).send(todo)
+    }, error => {
+        res.status(400).send(error);
+    });
+})
 
 app.listen(3000, () => {
     console.log('Server started on port : 3000');
