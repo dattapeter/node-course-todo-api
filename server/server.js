42012abcd1245045591fa377e4a2106e5d4592b2
Dattatray <dattapeter@gmail.com>
2019-02-03 16:45:54 +0530

    Auth Login

--- server/server.js
+++ server/server.js
@@ -101,19 +101,33 @@ app.patch('/todos/:id', (req, res) => {
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
-
+  
   user.save().then(() => {
     return user.generateAuthToken();
   }).then((token) => {
-      res.header('x-auth', token).send(user);
+    res.header('x-auth', token).send(user);
   }).catch(e => {
-      res.status(400).send(e);
+    res.status(400).send(e);
   })
 });
 
 app.get('/users/me', authenticate, (req, res) => {
-    res.send(req.user);
+  res.send(req.user);
 });
+
+
+app.post('/users/login', (req, res, next) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  
+  User.findByCredentials(body.email, body.password).then((user) => {
+    user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    })
+  }).catch((e) => {
+    res.send(400).send();
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
